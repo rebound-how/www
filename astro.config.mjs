f7aa5c7d49e2f47577854a8e0f396a2d9cb834a0
Marc Perrien <marc@bluelimes.dev>
2025-03-10 15:28:23 +0100

    Robots (#35)
    * Create robots.txt
    Contributes to issue #30
    
    Signed-off-by: Marc Perrien <marc@bluelimes.dev>
    
    * Install sitemap integration
    
    Signed-off-by: Marc Perrien <marc@bluelimes.dev>
    
    * Link to sitemap in robots.txt
    
    Signed-off-by: Marc Perrien <marc@bluelimes.dev>
    
    * Excluse message sent page from sitemap
    
    Signed-off-by: Marc Perrien <marc@bluelimes.dev>
    
    * Disallow indexing message sent page.
    Closes #31
    
    Signed-off-by: Marc Perrien <marc@bluelimes.dev>
    
    ---------
    
    Signed-off-by: Marc Perrien <marc@bluelimes.dev>

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,15 @@ import { defineConfig } from "astro/config";
 
 import icon from "astro-icon";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://rebound.how",
-  integrations: [icon()],
+  integrations: [
+    icon(),
+    sitemap({
+      filter: (page) => page !== "https://rebound.how/support/sent/",
+    }),
+  ],
 });
