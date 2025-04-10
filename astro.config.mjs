fafa3ad2aab8268974aa07da23bce46391545eab
Marc Perrien <marc@bluelimes.dev>
2025-04-10 15:21:50 +0200

    Add articles and MDX support
    Signed-off-by: Marc Perrien <marc@bluelimes.dev>

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,12 @@ import icon from "astro-icon";
 
 import sitemap from "@astrojs/sitemap";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://rebound.how",
-  integrations: [
-    icon(),
-    sitemap({
-      filter: (page) => page !== "https://rebound.how/support/sent/",
-    }),
-  ],
+  integrations: [icon(), sitemap({
+    filter: (page) => page !== "https://rebound.how/support/sent/",
+  }), mdx()],
 });
---
+++ src/data/articles/mardown-article/index.md
@@ -0,0 +1,7 @@
+---
+title: This article is Markdown
+description: Meta descriptions are important
+pubDate: 2025-04-10
+---
+
+hello
